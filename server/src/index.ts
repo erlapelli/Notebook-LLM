3cd0f3af0478d9f63d0f892612b5159936301cba
Erlapelli Poornachandhar <erlapellipoornachandhar30@gmail.com>
2026-09-07 13:08:03 +0530

    added services

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import "dotenv/config";
 import { toNodeHandler } from "better-auth/node";
 import { registerRoutes } from "./routes/index.js";
 import { errorHandler } from "./middleware/handler.middleware.js";
+import { inngest } from "./inngest/client.js";
+import { functions } from "./inngest/index.js"
+import { serve } from "inngest/express";
 import { auth } from "./lib/auth.js";
 import cors from "cors";
 
@@ -22,8 +25,10 @@ app.all("/api/auth/{*any}", toNodeHandler(auth));
 // Mount body-parsing middleware after the Better Auth handler.
 app.use(express.json());
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World Note");
 })
 
 app.get("/health", (req, res) => {
